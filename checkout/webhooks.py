f11f82ceb4240acd0ee6d4f1228055ad21fb0473
Pegrume88 <pegrume88@gmail.com>
2023-01-06 08:44:56 +0000

    added checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,4 +1,3 @@
-
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
@@ -34,9 +33,6 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    print('Success!')
-    return HttpResponse(status=200)
-
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
@@ -56,5 +52,3 @@ def webhook(request):
     # Call the event handler with the event
     response = event_handler(event)
     return response
-
-    
